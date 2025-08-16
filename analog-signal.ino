582172a0800ddc96bd32ead3e436f508a09f519d
La Quý Trung Nguyên <dabest0123456@gmail.com>
2025-08-16 15:20:56 +0700

    Update analog-signal.ino

--- analog-signal.ino
+++ analog-signal.ino
@@ -16,24 +16,26 @@ void loop() {
   int value = analogRead(analog);
   Serial.print("Gia tri Analog: ");
   Serial.print(value);
-  temperature = map(value,0,1023,0,100);
+  temperature = map(value, 0, 1023, 0, 100);
   Serial.print("  - Gia tri nhiet do: ");
   Serial.println(temperature);
   delay(2000);
   if (temperature < 30) {
-    digitalWrite(led[i], LOW);
+    digitalWrite(led[0], LOW);
+    digitalWrite(led[1], LOW);
+    digitalWrite(led[2], LOW);
   }
-  if ((temperature <= 50) and (temperature >= 30)) {
+  else if ((temperature <= 50) and (temperature >= 30)) {
     digitalWrite(led[0], HIGH);
     digitalWrite(led[1], LOW);
     digitalWrite(led[2], LOW);
   }
-  if ((temperature >= 51) and (temperature <= 80)) {
+  else if ((temperature >= 51) and (temperature <= 80)) {
     digitalWrite(led[0], LOW);
     digitalWrite(led[1], HIGH);
     digitalWrite(led[2], LOW);
   }
-  if (temperature >= 81) {
+  else if (temperature >= 81) {
     digitalWrite(led[0], LOW);
     digitalWrite(led[1], LOW);
     digitalWrite(led[2], HIGH);
